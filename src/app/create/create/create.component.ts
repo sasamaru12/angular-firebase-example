6a3878782a5fda83c501a391c468e1971516adb7
笹森祐太 <sasa2000yu@icloud.com>
2019-12-26 18:15:57 +0900

    topPage実装

---
+++ src/app/create/create/create.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.scss']
+})
+export class CreateComponent implements OnInit {
+
+  petIds = [...Array(10)].map((_, i) => i + 1);
+  // ...Array 特定の回数だけ空の配列を定義する
+  // .map 空の配列を取り出して"_=何もない" を i にする
+  config: SwiperConfigInterface = {
+    loop: true,
+    // 画像ループさせる？
+    navigation: true,
+    // 矢印だす？
+    pagination: true,
+    centeredSlides: true,
+    slidesPerView: 3
+  };
+  selectedPedId = 0;
+  form = this.fb.group({
+    name: ['', [
+      Validators.required,
+      Validators.maxLength(40)
+    ]],
+    gender: ['', [
+      Validators.required,
+      Validators.pattern(/male|female/)
+    ]]
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
+
+}
